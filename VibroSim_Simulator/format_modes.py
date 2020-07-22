fc6a10f6171412b7165b3cb12e7388693b3dfa30
Steve Holland <sdh4@iastate.edu>
2020-07-22 15:35:33 -0500

    More COMSOL 5.5 compatibility

--- VibroSim_Simulator/format_modes.py
+++ VibroSim_Simulator/format_modes.py
@@ -6,7 +6,16 @@ from . import read_comsol_probe_txt
 
 def print_comsol_modes(filename):
     (metadata,fieldheaderdata,fieldrowdata) = read_comsol_probe_txt.read(filename)
-    complex_freqs_raw=fieldrowdata[0]['freq (1/s)'][1]
+
+    if "freq (1/s)" in fieldrowdata[0]:
+        complex_freqs_raw=fieldrowdata[0]['freq (1/s)'][1]
+        pass
+    elif "freq (Hz)" in fieldrowdata[0]: # COMSOL 5.5 gives freq in Hz instead of 1/s as column label
+        complex_freqs_raw=fieldrowdata[0]['freq (Hz)'][1]
+        pass
+    else: 
+        raise ValueError("Did not find field row data for frequencies (candidates=%s)" % (str(list(fieldrowdata[0].keys()))))
+
     print("Frequency (Hz)   1/e Decay time (s)")
     print("-----------------------------------")
     print("\n".join(["%15.2f  %15.2f" % (freqval.real,1.0/(2.0*np.pi*freqval.imag)) for freqval in complex_freqs_raw])) 
